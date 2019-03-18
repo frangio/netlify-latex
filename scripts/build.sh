c5773a82d7dfa3cf28f2d6ba03734110b6e4481b
Francisco Giordano <frangio.1@gmail.com>
2019-03-17 22:35:36 -0300

    fix PATH in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,7 +17,9 @@ TEXLIVE_BIN="$TEXLIVE/bin/x86_64-linux"
 
 TEXLIVEONFLY="$TEXLIVE/texmf-dist/scripts/texliveonfly/texliveonfly.py"
 
-python "$TEXLIVEONFLY" --texlive_bin="$TEXLIVE_BIN" -c latexmk -a -pdf journal.tex
+export PATH="$TEXLIVE_BIN:$PATH"
+
+python "$TEXLIVEONFLY" -c latexmk -a -pdf journal.tex
 
 mkdir -p dist
 
